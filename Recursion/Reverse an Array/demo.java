8bfc62593f45e65ed0a46ea6b304df08b613925f
anujkatare <anujkatare2006@gmail.com>
2026-02-10 15:47:35 +0530

    Reverse an Array using Recursion

---
+++ Recursion/Reverse an Array/demo.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class demo {
+    public int f(int l, int r, int array[]) {
+        if(l >= r){
+            return 0 ;
+        }
+
+        int temp = array[l];
+        array[l] = array[r];
+        array[r] = temp;
+
+         return f(l+1, r-1, array);
+        
+    }
+    public static void main(String[] args) {
+       int[] array = {10,30,20,40,55};
+       demo d = new demo();
+       d.f(0, 4, array);
+       System.out.println(Arrays.toString(array));
+     }
+}
