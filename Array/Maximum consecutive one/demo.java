6fee0fadc4bc2ac725e3bfc82590c0f938c27db4
anujkatare <anujkatare2006@gmail.com>
2026-02-21 22:40:05 +0530

    Maximum Consecutive Ones

---
+++ Array/Maximum consecutive one/demo.java
@@ -0,0 +1,18 @@
+public class demo {
+    public static void main(String[] args) {
+        int array[] = {1,1,1,0,0,0,1,1,1,1,0,0,1,1,1,0};
+        int count = 0;
+        int max = 0;
+    
+        for(int i = 0; i < array.length; i++){
+           if(array[i] == 1){
+              count++;
+              max = Math.max(max, count);
+           }else{
+            count = 0;
+           }
+        }
+
+        System.out.println(max);
+    }
+}
